44edc75b332567124ead65e4b76059df5337b3a8
namu56 <swi920224@gmail.com>
2024-06-23 16:45:36 +0900

    fix: second commit

--- app/index.test.ts
+++ app/index.test.ts
@@ -1,17 +1,16 @@
-import request from 'supertest';
-import { LIST_KEY, RedisClient, createApp } from './app';
-import { App } from 'supertest/types';
-import * as redis from 'redis';
+import request from "supertest";
+import { LIST_KEY, RedisClient, createApp } from "./app";
+import * as redis from "redis";
 
-let app: App;
+let app: Express.Application;
 let client: RedisClient;
 
-const TEST_REDIS_URL = 'redis://:test_env@localhost:6380';
+const REDIS_URL = "redis://default:test_env@localhost:6380";
 
 beforeAll(async () => {
-  client = redis.createClient({ url: TEST_REDIS_URL });
+  client = redis.createClient({ url: REDIS_URL });
   await client.connect();
-  app = await createApp(client);
+  app = createApp(client);
 });
 
 beforeEach(async () => {
@@ -23,23 +22,22 @@ afterAll(async () => {
   await client.quit();
 });
 
-describe('POST /message', () => {
-  it('responds with a success message', async () => {
-    const response = await request(app).post('/messages').send({
-      message: 'testing with redis',
-    });
+describe("POST /messages", () => {
+  it("responds with a success message", async () => {
+    const response = await request(app)
+      .post("/messages")
+      .send({ message: "testing with redis" });
+
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Message added to list');
+    expect(response.text).toBe("Message added to list");
   });
 });
 
-describe('GET /messages', () => {
-  it('responds with all messages', async () => {
-    await client.lPush(LIST_KEY, ['msg1', 'msg2']);
-    const response = await request(app).get('/messages');
+describe("GET /messages", () => {
+  it("responds with all messages", async () => {
+    await client.lPush(LIST_KEY, ["msg1", "msg2"]);
+    const response = await request(app).get("/messages");
     expect(response.statusCode).toBe(200);
-
-    const messages = await client.lRange(LIST_KEY, 0, -1);
-    expect(response.body).toEqual(messages);
+    expect(response.body).toEqual(["msg2", "msg1"]);
   });
 });
